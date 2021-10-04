60203d1d5c2600c0fce63f964ed16bd1e4cfa724
juanx120 <guerrerojuancho78@gmail.com>
2021-10-04 17:10:06 -0500

    Se agregaron las ids unicas de cada boton de la tienda

--- js/Tienda.js
+++ js/Tienda.js
@@ -4,18 +4,33 @@ var db = firebase.firestore();
 
 var tabla = document.getElementById('producto')
 
-db.collection('Productos').get().then((snapshot) => {
-    tabla.innerHTML = '';
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-        tabla.innerHTML += `
-        <div class="contenido">
-            <img class="imgproduct" src="${doc.data().Imagen}" alt="">
-            <h4>${doc.data().Nombre}</h4> 
-            <h5>$${doc.data().Precio}</h5> 
-            <a class="agregar">Detalles</a>
-            <button class="boton_agregar">Agregar al carrito</button>
-        </div>
-        `
-    })
-});
+
+
+window.addEventListener('DOMContentLoaded', (e) =>{
+    db.collection('Productos').get().then((snapshot) => {
+        tabla.innerHTML = '';
+        snapshot.forEach((doc) => {
+            console.log(doc.id, '=>', doc.data());
+            tabla.innerHTML += `
+            <div class="contenido">
+                <img class="imgproduct" src="${doc.data().Imagen}" alt="">
+                <h4>${doc.data().Nombre}</h4> 
+                <h5>$${doc.data().Precio}</h5> 
+                <a class="detalles">Detalles</a>
+                <button class="boton_agregar" data-id="${doc.id}">Agregar al carrito</button>
+            </div>
+            `;
+
+            const btn_agregar = document.querySelectorAll('.boton_agregar')
+                btn_agregar.forEach(btn =>{
+                    btn.addEventListener('click', (e)=>{
+                    console.log(e.target.dataset.id)
+                })
+            })
+
+        })
+    });
+})
+
+
+
