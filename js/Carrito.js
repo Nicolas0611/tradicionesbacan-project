9d2947fdf81f33b050ecb24824b2756abd478304
NickArc <nickarc19@gmail.com>
2021-10-08 21:41:29 -0500

    Carrito funcional, falta implementacion local storage

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,84 +1,94 @@
 var db = firebase.firestore();
-var tabla = document.getElementById('Carrito');
-var visibilidad = 1;
-const Carrito = document.getElementById('carritoFinal');
-const Prods = db.collection('Productos').get();
-const listaProd = document.querySelector('#lista-carrito tbody')
-const vaciarCarritoBoton = document.getElementById('vaciar-carrito');
-cargarEventListeners();
+var Carrito = document.getElementById('Carrito');
+var tabla = document.getElementById('producto');
 
-function cargarEventListeners(){
-    Prods.addEventListener('click',comprarProducto);
-    Carrito.addEventListener('click',eliminarProducto);
-    vaciarCarritoBoton.addEventListener('click', vaciarCarrito);
-    document.addEventListener('DOMContentLaoded', leerLocalStorage);
-}
-function comprarProducto(e){
-    e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const producto = e.target.parentElement.parentElement;
-        leerDatosProducto(producto);
-    }
-}
-function leerDatosProducto(producto){
-    const infoProducto={
-        imagen: producto.querySelector('img').src,
-        titulo: producto.querySelector('h4').textContent,
-        precio: producto.querySelector('h5').textContent,
-        id: producto.querySelector('a').getAttribute('data-id')
-    }
-    insertarCarrito(infoProducto);
-}
-function insertarCarrito(producto){
-    const row = document.createElement('tr')
-    row.innerHTML=`
-        <td>
-            <img src"${producto.imagen}" width="100">
-        </td>
-        <td>${producto.titulo}</td>
-        <td>${producto.precio}</td>
-        <td>
-            <a href="#" class="borrar" data-id="${producto.id}">X</a>
-        </td>
-    `;
-    listaProd.appendChild(row);
-    guardarProductosLocalStorage(producto);
-}
-function eliminarProducto(e){
-    e.preventDefault();
-    let producto,
-    prductoId;
-    if(e.target.classList.contains('borrar-producto')){
-        e.target.parentElement.parentElement.remove();
-        producto= e.target.parentElement.parentElement;
-        productoId
-    }
-}
-/*function mostrarCarrito() {
-    if (visibilidad == 1) {
-        visibilidad = 2;
-        document.getElementById('Carrito').style.display = 'block';
-    }
-    else if (visibilidad == 2) {
-        visibilidad = 1;
-        document.getElementById('Carrito').style.display = 'none';
-    }
-}*/
-
-
-/*db.collection('Productos').get().then((snapshot) => {
-    //tabla.innerHTML = '';
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-        tabla.innerHTML += `
-        <div class="FotoCarrito">
-            <img src="${doc.data().Imagen}" alt=""/>
-            <h5> ${doc.data().Nombre}<br>$ ${doc.data().Precio}</h5>
-            <div class="Contador">
-                <form class="agregar"><input type="number" id="cantidad" name="cantidad" min="1" placeholder="Cantidad">
-            </div>
-            <h5>Total:<br>$100000</h5>
-        </div>
-        `
+db.collection('Productos').get().then((snapshot) => {
+    const btn_agregar = document.querySelectorAll('.boton_agregar')
+    btn_agregar.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            console.log(e.target.dataset.id)
+        })
     })
-});*/
+    btn_agregar.forEach(btn_agregar_carro => {
+        btn_agregar_carro.addEventListener('click', btn_agregar_clicked);
+    })
+});
+const ContenedoresDeItems = document.querySelector('#Carrito2');
+function btn_agregar_clicked(event) {
+    const button = event.target;
+    const item = button.closest('.contenido');
+    const itemTitulo = item.querySelector('.contenido h4').textContent;
+    const itemPrecio = item.querySelector('.contenido h5').textContent;
+    const itemImagen = item.querySelector('.contenido img').src;
+    addItemAlCarrito(itemTitulo, itemPrecio, itemImagen);
+}
+function addItemAlCarrito(itemTitulo, itemPrecio, itemImagen) {
+
+    const elementsTitle = ContenedoresDeItems.getElementsByClassName('ItemTitulo')
+    for (let i = 0; i < elementsTitle.length; i++) {
+        if (elementsTitle[i].innerText === itemTitulo) {
+            const elementQuantity = elementsTitle[i].parentElement.parentElement.querySelector('.ItemCantidad');
+            elementQuantity.value++;
+            ActualizarTotal();
+            return;
+        }
+    }
+    const FilaCarrito = document.createElement('div');
+    const ContenidoCarrito = `
+    <table class= "lista-carrito" class="u-full-width">
+        <thead>
+        <tr>
+            <th>Imagen</th>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class ="ItemCarrito">
+            <td>
+            <img src=${itemImagen} width="100px">
+            </td>
+            <td class="ItemTitulo">${itemTitulo}</td>
+            <td class="ItemPrecio">${itemPrecio}</td>
+            <td><input class="ItemCantidad" type="number" min= "1" max="100" value="1"></td>
+            <td><a href="javascript:void(0);" class="borrar-producto">X</a></td>
+        </tr>
+        </tbody>
+    </table>
+    `;
+    FilaCarrito.innerHTML = ContenidoCarrito;
+    ContenedoresDeItems.append(FilaCarrito);
+
+    FilaCarrito.querySelector('.borrar-producto').addEventListener('click', removeShoppingCartItem);
+    FilaCarrito.querySelector('.ItemCantidad').addEventListener('change',quantityChanged)
+    ActualizarTotal();
+}
+function ActualizarTotal() {
+    let Total = 0;
+    const TotalCarrito = document.querySelector('.total-carrito');
+    const ItemsCarrito = document.querySelectorAll('.ItemCarrito');
+
+
+    ItemsCarrito.forEach(ItemCarrito => {
+        const shoppingCartItemPriceElement = ItemCarrito.querySelector('.ItemPrecio');
+        const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('$', ''));
+        const shoppingCartItemQuantityElement = ItemCarrito.querySelector('.ItemCantidad');
+        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+        Total = Total + shoppingCartItemPrice * shoppingCartItemQuantity;
+    });
+    TotalCarrito.innerHTML = `$${Total.toFixed(2)}`
+}
+function removeShoppingCartItem(event) {
+    const buttonClicked = event.target;
+    buttonClicked.closest('.lista-carrito').remove();
+    ActualizarTotal();
+}
+function quantityChanged(event) {
+    const input = event.target;
+    if (input.value <= 0) {
+        input.value = 1;
+    }
+    input.value <= 0 ? (input.value = 1) : null;
+    ActualizarTotal();
+}
