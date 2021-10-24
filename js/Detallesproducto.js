330a8a5cc6865ffb4d3e5fd58ffa2a3735e4f3f8
DavidArguello1321 <est.david.arguello@unimilitar.edu.co>
2021-10-24 11:14:21 -0500

    pagina admin

--- js/Detallesproducto.js
+++ js/Detallesproducto.js
@@ -1,47 +1,41 @@
-function escribirmodal(id_producto){
+function escribirmodal(id_producto) {
     console.log("id producto", id_producto);
-    titulomodal.innerHTML='';
-    modalcontenidoI.innerHTML='';
-    modaldescripcion.innerHTML='';
-    precio.innerHTML='';
-    db.collection("Productos").doc(id_producto).get().then((doc)=>{
-        titulomodal.innerHTML=`
+    titulomodal.innerHTML = '';
+    modalcontenidoI.innerHTML = '';
+    modaldescripcion.innerHTML = '';
+    precio.innerHTML = '';
+    db.collection("Productos").doc(id_producto).get().then((doc) => {
+        titulomodal.innerHTML = `
         ${doc.id}: ${doc.data().Nombre}
         `;
-        modalcontenidoI.innerHTML=`
+        modalcontenidoI.innerHTML = `
             <img class="imgproductD" src="${doc.data().Imagen}" alt="">
         `;
 
-        precio.innerHTML=`
+        precio.innerHTML = `
             <b>Precio: ${doc.data().Precio}</b>
         `
-        
-        botonesdetalles.innerHTML=`
+
+        botonesdetalles.innerHTML = `
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             <button type="button" class="boton_agregar btn btn-primary" data-id="${doc.id}" data-bs-dismiss="modal">Agregar al carrito</button>
         `
 
         const btn_agregar = document.querySelectorAll('.boton_agregar')
-        btn_agregar.forEach(btn =>{
-            btn.addEventListener('click', (e)=>{
-            console.log(e.target.dataset.id)
+        btn_agregar.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                console.log(e.target.dataset.id)
+            })
         })
-    })
 
-    console.log(doc.data().Descripcion.length)
+        console.log(doc.data().Descripcion.length)
 
-        for(var i=0;i<doc.data().Descripcion.length;i++)
-        {
-            modaldescripcion.innerHTML+=`
+        for (var i = 0; i < doc.data().Descripcion.length; i++) {
+            modaldescripcion.innerHTML += `
                 <li>
                 ${doc.data().Descripcion[i]}
                 </li>
             `
         }
-
-        
-        
-
-
     });
 }
